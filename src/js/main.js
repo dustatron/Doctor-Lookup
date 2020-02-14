ca0b930fb2a02e75216570d69d989495eab34f67
Dusty <dustymccord@gmail.com>
2020-02-14 12:19:07 -0800

    add a loading gif.

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
     event.preventDefault();
     let inputKeyWord = $("#key-word").val();
     let inputDocName = $("#doctor-name").val();
+    $(".output").html("");
+    $(".loading").show();
 
     //Check what input has data, if no data: return error, if data in both: return error
     if (inputKeyWord && inputDocName) {
@@ -30,18 +32,22 @@ $(document).ready(function() {
       //run query for Doctor Name
       if (option === "name") {
         const docList = await getDoctor.byName(inputDocName);
+        $(".loading").hide();
         if (docList.length > 0) {
           ui.renderList(docList);
         } else {
+          $(".loading").hide();
           $(".output").html("Zero search results");
         }
         //run query for Keyword
       } else if (option === "keyWord") {
         const docList = await getDoctor.byKeyWord(inputKeyWord);
         if (docList.length > 0) {
+          $(".loading").hide();
           ui.renderList(docList);
           //report issue
         } else {
+          $(".loading").hide();
           $(".output").html("Zero search results");
         }
       }
